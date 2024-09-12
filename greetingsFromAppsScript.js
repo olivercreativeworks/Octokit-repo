93a18c92a47686e1331074423ce620ae185a4cf4
olivercreativeworks <76704559+olivercreativeworks@users.noreply.github.com>
2024-09-12 11:51:05 -0400

    Greeting v2. Now with an extension :)

---
+++ greetingsFromAppsScript.js
@@ -0,0 +1,9 @@
+/** 
+ * This is a greeting v2.
+ * 
+ * The first one forgot the extension...
+ */
+function secondGreeting() {
+  console.log('This message is brought to you by my apps script project!')
+  console.log('Pretty cool huh?')
+}
